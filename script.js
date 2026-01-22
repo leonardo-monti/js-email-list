ef8009ab0b01f05c6d688963c9618364d5df0b19
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-22 16:25:10 +0100

    end

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const mailList = document.getElementById("mail-list")
 
 const apiUrl = "https://flynn.boolean.careers/exercises/api/random/mail"
+
+for (let i = 0; i < 10; i++) {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const li = document.createElement("li")
+            li.textContent = data.response
+            mailList.appendChild(li)
+
+        })
+}
